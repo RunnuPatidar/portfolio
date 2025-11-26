669ad5b8c88bb391967a390f6f351815dba30c0b
RunnuPatidar <runjhunpatidar11@gmail.com>
2025-11-26 17:39:15 +0530

    Add files via upload

---
+++ page2.js
@@ -0,0 +1,8 @@
+ 
+const toggle = document.getElementById('navToggle');
+const menu = document.getElementById('navbarMenu');
+
+toggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+ 
